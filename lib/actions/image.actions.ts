968a0a96059e22e2dc78a7d2bdd74d3f7de05ce8
Curtis <chartshorne@gmail.com>
2024-03-18 12:27:39 -0700

    initial codebase complete

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -113,7 +113,7 @@ export async function getAllImages({
       secure: true,
     });
 
-    let expression = "folder=imaginify";
+    let expression = "folder=imaginize";
 
     if (searchQuery) {
       expression += ` AND ${searchQuery}`;
